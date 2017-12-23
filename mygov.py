b2c119f94f6017caa7b307921fa2e68681debc85
SHAIK KAMRAN <shaikkamran3@gmail.com>
2017-12-23 23:36:14 +0530

    Add files via upload

---
+++ mygov.py
@@ -0,0 +1,41 @@
+# Scrapes result from http://result.vtu.ac.in using BeautifulSoup
+
+from bs4 import BeautifulSoup
+
+import requests
+url="https://innovate.mygov.in/sih2018/"
+reqfile = requests.get(url)
+soup1 = BeautifulSoup(reqfile.content,"html.parser")
+#Find all the links in the myGov Site 
+
+links=soup1.find_all('div',{'class':"thumbnail"})
+
+for newurl in links:
+    reqfile1= requests.get(newurl.find('a')['href'])
+    soup=BeautifulSoup(reqfile1.content,"html.parser")
+    
+    
+    search=soup.find_all('div',{'class':"search-by"})
+    content=soup.find_all('div',{'class':"description"})
+    problem_list=problem_list=soup.find_all('div',{'class':"head-part"})
+
+    for i in search:
+        print(i.text)
+
+    print("\n\n")
+    cnt=0
+
+    for i,j in zip(problem_list,content):
+        cnt+=1
+        print(cnt,end="\t\t")
+        print(i.a.text)
+        print()
+        print(j.p.text)
+        print("\n\n")
+
+
+    print("\n\n\n\n\n\n")
+        
+    
+
+    
